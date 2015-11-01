8d86be4ff2fa0706965f5559c33e467d6bdd7be9
patmaz <patrykiewicz@gmail.com>
2015-11-01 10:44:50 +0100

    add js

--- index.php
+++ index.php
@@ -6,7 +6,6 @@
  <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
  <meta name="viewport" content="width=device-width, initial-scale=1" /> 
  
-
 <link rel="stylesheet" href="css/main.css" type="text/css" />
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
@@ -14,10 +13,13 @@
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+<script src="js/main.js"></script>    
+<script src="js/videobg.js"></script>    
 </head>
-
-
 <body>
 
+    
+    
 </body>
 </html>
