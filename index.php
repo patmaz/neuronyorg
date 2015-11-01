bb1f0e959db2dd3e38c5e935921df81048b5e7ae
patmaz <patrykiewicz@gmail.com>
2015-11-01 19:13:04 +0100

    add nav

--- index.php
+++ index.php
@@ -15,15 +15,59 @@
 <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 
 <script src="js/main.js"></script>    
-<script src="js/videobg.js"></script>
 <script src="js/vide.js"></script>
 </head>
 <body>
 
-<div id="wrapper" data-vide-bg="webm: vidz/brain.webm">
+<div id="wrapper" data-vide-bg="">    
+<div id="wrapper2">
     
     
-
+<div class="row">
+    <div class="col-md-4">
+        <div class="sidebar-nav">
+        <div class="navbar navbar-inverse">
+          <div class="container">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="#">Project name</a>
+            </div>
+            <div id="navbar" class="navbar-collapse collapse">
+              <ul class="nav navbar-nav">
+                    <li><a href="#div1">Page 1</a>
+                    </li>
+                    <li><a href="#div2">Page 2</a>
+                    </li>
+                    <li><a href="#div3">Page 3</a>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Action</a></li>
+                            <li><a href="#">Another action</a></li>
+                            <li><a href="#">Something else here</a></li>
+                            <li class="divider"></li>
+                            <li class="dropdown-header">Nav header</li>
+                            <li><a href="#">Separated link</a></li>
+                            <li><a href="#">One more separated link</a></li>
+                        </ul>
+                    </li>
+                
+              </ul>
+            </div>
+          </div>
+        </div>
+    </div>
+    </div>
+    <div class="col-md-6"></div>
+</div>
+    
+</div>
 </div>
     
 </body>
--- js/main.js
+++ js/main.js
@@ -1,2 +1,23 @@
-var jq = $.noConflict();
+jQuery.noConflict(); 
+jQuery(document).ready( function(){ 
 
+    
+    function setBG(){
+        
+        if (jQuery(window).width() > 500){
+            
+            jQuery("#wrapper").attr("data-vide-bg", "webm: vidz/brain.webm");
+            
+        } else {
+            
+            jQuery("#wrapper").attr("data-vide-bg", "poster: img/brain.jpg");
+            
+        }
+
+    }
+    
+    setBG();
+    
+    
+    
+});  
